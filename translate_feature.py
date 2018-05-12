fe688e1216dffd74266c1658bff5172b67241e24
yuechaohou <yuechaohou@163.com>
2018-05-11 20:39:07 -0400

    edit

--- translate_feature.py
+++ translate_feature.py
@@ -18,25 +18,9 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.cluster import KMeans
 import numpy as np
+import pandas as pd
 
 sns.set(color_codes=True)
-
-
-# In[2]:
-
-
-movie_dict = movie.get_info_with_localname('Black Panther(2018)')
-print(movie_dict['summary']['Genre'])
-budget = movie_dict['summary']['Budget']
-revenue = movie_dict['international']['Worldwide Box Office'][0]['Revenue']
-genre = movie_dict['summary']['Genre']
-# print(keywords)
-# print(news)
-ROI = (revenue - budget) / budget
-print(ROI)
-
-
-# In[3]:
 
 
 def is_released(movie_name, movie_infos):
@@ -63,8 +47,6 @@ def get_movie_names_all():
     movie_names = list(movie_infos.keys())
     movie_names = [movie_name for movie_name in movie_names if is_released(movie_name, movie_infos)]
     return movie_names
-
-print(get_movie_names_all()[:10])
 
 
 # In[4]:
@@ -102,14 +84,12 @@ def get_ROI(movie_name, all_dict):
     revenue = get_revenue(movie_name, all_dict)
     if revenue is None:
         return None
-    if movie_name == 'Deep Throat(1972)':
-        print(revenue, budget)
     ROI = (revenue - budget) / budget
     return ROI
 
 def get_ROIs():
     movie_names = get_movie_names_all()
-    print(movie_names[:10])
+    # print(movie_names[:10])
     
     with open('movie_info.pkl', 'rb') as f:
         movie_dict = pickle.load(f)
@@ -123,23 +103,6 @@ def get_ROIs():
     # output.sort(key=operator.itemgetter(1))
     # output.reverse()
     return output
-
-ROIs = get_ROIs()
-# print(ROIs[:100])
-
-
-# In[5]:
-
-
-# visualize the ROI distributions
-ROI_np = np.array([ROI[1] for ROI in ROIs])
-print(ROI_np[-1])
-print(ROI_np[200])
-print(ROI_np)
-
-sns.distplot(ROI_np[200:])
-plt.show()
-
 
 # The individual difference of the ROI of each movie could be very large, ranging from -0.9999749 to 1799. With regard to the movie with exceptionally large ROI, we regard these values as outliers and give special consideration. We divide our ROI range into the following groups: [-0.9999749, 0), [0, 1), [1, 2), [2, 5), [5, 10), [10, 30), [30, ..). Now, we use this boudary to label the different movies.
 
@@ -203,66 +166,6 @@ def get_labels_revenue():
         label = label_revenue(revenue)
         output.append( (movie_name, label) )
     return output
-    
-
-
-# In[11]:
-
-
-# get the distributions of the movies dataset using the ROI
-
-movie_names = get_movie_names_all()
-ROIs = get_ROIs()
-labels = get_labels_roi()
-revenues = get_revenues()
-labels_revenue = get_labels_revenue()
-
-print(labels[:10])
-print(labels_revenue[:10])
-
-file = open("matrix.txt", "w")
-for i in range(len(labels)):
-    string = labels[i][0] + ' ' + str(labels[i][1]) + ' ' + str(labels_revenue[i][1]) + '\n'
-    file.write(string)
-file.close()
-    
-
-statistics_revenue = {}
-for i in range(8):
-    statistics_revenue[i] = 0
-for movie, label in labels_revenue:
-    statistics_revenue[label] += 1
-print(statistics_revenue)
-
-print(len(revenues), len(ROIs))
-print(revenues[:10])
-print(ROIs[:10])
-
-# print(revenues[:10])
-print(get_labels_revenue())
-
-# find out the number of movies for each ROI category
-labels = get_labels_roi()
-statistics_roi = {}
-for i in range(7):
-    statistics_roi[i] = 0
-for movie, label in labels:
-    statistics_roi[label] += 1
-print(statistics_roi)
-
-w, h = 8, 7
-Matrix = [[[] for x in range(w)] for y in range(h)]
-Matrix_keywords = [[[] for x in range(w)] for y in range(h)]
-
-for i in range(len(revenues)):
-    row = labels[i][1]
-    col = labels_revenue[i][1]
-    Matrix[row][col].append(labels[i][0])
-# print(Matrix)
-
-
-# In[8]:
-
 
 # get the semantics of a comment
 def get_semantics(text):
@@ -272,49 +175,8 @@ def get_semantics(text):
     neu = ss['neu']
     compound = ss['compound']
     pos = ss['pos']
-    print(neg, neu, compound, pos)
+    # print(neg, neu, compound, pos)
     return neg, neu, compound, pos
-
-print(get_semantics("This is so great! haha"))
-
-
-# In[33]:
-
-
-ROIs = get_ROIs()
-ROI_np = np.array([ROI[1] for ROI in ROIs])
-ROI_np = np.reshape(ROI_np, (len(ROI_np), 1))
-
-test_np = sorted(ROI_np, reverse=True)
-ROI_np = test_np[1:]
-
-kmeans1 = KMeans(n_clusters=10, random_state=0).fit(ROI_np)
-print(kmeans1.cluster_centers_)
-print(kmeans1.labels_)
-
-test = dict([(i, 0) for i in range(len(np.unique(kmeans1.labels_)))])
-for label in kmeans1.labels_:
-    test[label] += 1
-print(test)
-
-Revenues = get_revenues()
-Revenue_np = np.array([Revenue[1] for Revenue in Revenues])
-Revenue_np = np.reshape(Revenue_np, (len(Revenue_np), 1))
-print(Revenue_np)
-
-kmeans2 = KMeans(n_clusters=3, random_state=0).fit(Revenue_np)
-print(Revenue_np[:10])
-print(kmeans2.cluster_centers_)
-print(kmeans2.labels_)
-# print(kmeans2.predict(Revenue_np[:10]))
-print(np.unique(kmeans2.labels_))
-test = dict([(i, 0) for i in range(len(np.unique(kmeans2.labels_)))])
-for label in kmeans2.labels_:
-    test[label] += 1
-print(test)
-
-
-# In[ ]:
 
 
 # prepare the data
@@ -325,20 +187,6 @@ def get_info_with_localname(name):
         name_dic = pickle.load(f)
     return name_dic[name]
 
-movie_names = ['Black Panther(2018)', '', '', '', '', '', '']
-movie_info = get_info_with_localname('Black Panther(2018)')
-box_offices = movie_info['box_office']['daliy']
-data = []
-for box_office in box_offices:
-    data.append([box_office['Gross']])
-    
-data = np.array(data)
-# print(data)
-
-# normalize
-# scaler = MinMaxScaler(feature_range=(0, 1))
-# data = scaler.fit_transform(data)
-
 def get_daily_box_offices(movie_name):
     movie_info = get_info_with_localname(movie_name)
     box_offices = movie_info['box_office']['daliy']
@@ -348,8 +196,6 @@ def get_daily_box_offices(movie_name):
     
     data = np.array(data)
     return data
-
-data = get_daily_box_offices('Black Panther(2018)')
 
 def data_partition(data, N):
     num_rows = data.shape[0]
@@ -364,3 +210,70 @@ def data_partition(data, N):
     x = np.array(x)
     y = np.array(y)
     return x, y
+
+
+from matplotlib import pyplot
+from sklearn.preprocessing import MinMaxScaler
+
+# In[ ]:
+
+
+
+# In[ ]:
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+def one_hot_encode(data):
+    n = len(np.unique(data))
+    result = np.zeros(shape=(len(data), n))
+    for index, sample in enumerate(data):
+        result[index][sample] = 1
+    return result
+
+
+def get_neural_mlc(input_dim, output_dim):
+    model = Sequential()
+    
+    model.add(Dense(256, activation=backend.relu, input_dim=input_dim))
+    # model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(128, activation=backend.relu))
+    # model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(64, activation=backend.relu))
+    # model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+
+    model.add(Dense(output_dim))
+    model.add(Activation('softmax'))
+    
+    # print out the network architecture
+    model.summary()
+    
+    model.compile(loss='categorical_crossentropy', 
+              optimizer='adam',
+              metrics=['accuracy'])
+    
+    return model
+
+def get_model_regression(input_dim):
+    model = Sequential()
+    model.add(Dense(20, input_dim=input_dim, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(10, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(5, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(1, kernel_initializer='normal'))
+    # Compile model
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+def run_neural_mlc(X_train, X_test, y_train, y_test, epochs):
+    model = get_neural_mlc(X_train.shape[1], len(y_train[0]))
+    model.fit(X_train, y_train, epochs=epochs)
+    predictions = model.predict(X_test)
+    count = 0
+    for index, prediction in enumerate(predictions):
+        # print(np.argmax(prediction), np.argmax(y_test[index]))
+        count += (np.argmax(prediction) == np.argmax(y_test[index]))
+    accuracy = count / len(y_test)
+    return model, accuracy
